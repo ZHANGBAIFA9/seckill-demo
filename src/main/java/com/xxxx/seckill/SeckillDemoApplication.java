1e4597c9cae9e05066a3aaf28eb692f68ba8f079
ZHANGBAIFA9 <1350862999@qq.com>
2021-07-24 15:44:49 +0800

    redis分布式锁

--- src/main/java/com/xxxx/seckill/SeckillDemoApplication.java
+++ src/main/java/com/xxxx/seckill/SeckillDemoApplication.java
@@ -1,8 +1,11 @@
 package com.xxxx.seckill;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.redisson.Redisson;
+import org.redisson.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.xxxx.seckill.pojo")
@@ -10,6 +13,28 @@ public class SeckillDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SeckillDemoApplication.class, args);
+    }
+
+
+//    @Autowired
+//    private Environment env;
+//
+//    @Bean(destroyMethod = "shutdown")
+//    public RedissonClient redissonClient() throws IOException {
+//        String[] profiles = env.getActiveProfiles();
+//        String profile = "";
+//        if(profiles.length > 0) {
+//            profile = "-" + profiles[0];
+//        }
+//        return Redisson.create(
+//                Config.fromYAML(new ClassPathResource("redisson" + profile + ".yml").getInputStream())
+//        );
+//    }
+    @Bean()
+    public Redisson redisson(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://localhost:6379").setDatabase(0);
+        return (Redisson) Redisson.create(config);
     }
 
 }
--- src/main/java/com/xxxx/seckill/controller/DemoController.java
+++ src/main/java/com/xxxx/seckill/controller/DemoController.java
@@ -1,8 +1,17 @@
 package com.xxxx.seckill.controller;
 
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Version 1.0
@@ -15,12 +24,58 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DemoController {
     /**
      * 测试页面跳转
+     *
      * @param model
      * @return
      */
     @RequestMapping("/hello")
-    public String hello(Model model){
-        model.addAttribute("name","xxxxx") ;
-        return "hello" ;
+    public String hello(Model model) {
+        model.addAttribute("name", "xxxxx");
+        return "hello";
     }
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+    @Autowired
+    private Redisson redisson ;
+
+    @RequestMapping("/destuck")
+    @ResponseBody
+    public String destuck() {
+//        synchronized (this){
+        //分布式锁
+        String lockKey = "product_01" ;
+        String clientId = UUID.randomUUID().toString() ; //解决自己的锁被别人释放掉的场景
+//        Boolean result = redisTemplate.opsForValue().setIfAbsent(lockKey, "baifa");
+//        redisTemplate.expire(lockKey,10,TimeUnit.SECONDS) ;
+        //是上面两行代码的合并
+        Boolean result = redisTemplate.opsForValue().setIfAbsent(lockKey,clientId,10,TimeUnit.SECONDS);
+        if(!result){
+            return "error_code" ;
+        }
+        RLock redissonLock = redisson.getLock(lockKey);
+        try {
+            redissonLock.lock();
+            int stock = Integer.parseInt(redisTemplate.opsForValue().get(lockKey));
+            if (stock > 0) {
+                int realStock = stock - 1;
+                redisTemplate.opsForValue().set("stock", realStock + "");
+                System.out.println("扣减成功，剩余库存：" + realStock);
+            } else {
+                System.out.println("扣减失败，库存不足");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {//释放锁
+            //判断当前锁是否是自己拥有的锁
+//            if(clientId.equals(redisTemplate.opsForValue().get(lockKey))){
+//                //程序卡顿仍然会出现释放掉别人的锁问题
+//                redisTemplate.delete(lockKey) ;
+//            }
+            redissonLock.unlock();
+        }
+//        }
+        return "stock：";
+    }
+
 }
